4d2c1b7c25885cf740cb35fbaa03d3e2e807cd60
peerless_hero <peerless_hero@outlook.com>
2024-05-07 01:41:59 +0800

    feat: 初始化提交

---
+++ template/packages/openapi-v3/index.mjs
@@ -0,0 +1,12 @@
+/*
+ * @Author: peerless_hero peerless_hero@outlook.com
+ * @Date: 2024-05-04 13:22:47
+ * @LastEditors: peerless_hero peerless_hero@outlook.com
+ * @LastEditTime: 2024-05-04 13:24:29
+ * @FilePath: \cli\packages\openapi-v3\index.js
+ * @Description:
+ *
+ */
+import json from './OpenAPIV3.json'
+
+export const OpenAPIV3 = json
