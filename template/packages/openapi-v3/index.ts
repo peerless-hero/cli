4e94ae5a1c432ec19425e3b50723c8954d34ef1a
peerless_hero <peerless_hero@outlook.com>
2024-05-08 03:41:23 +0800

    fix: 文件后缀

--- template/packages/openapi-v3/index.ts
+++ template/packages/openapi-v3/index.ts
@@ -2,12 +2,12 @@
  * @Author: peerless_hero peerless_hero@outlook.com
  * @Date: 2024-05-04 13:22:47
  * @LastEditors: peerless_hero peerless_hero@outlook.com
- * @LastEditTime: 2024-05-07 21:49:00
- * @FilePath: \cli\src\template\packages\openapi-v3\index.ts
+ * @LastEditTime: 2024-05-08 03:20:24
+ * @FilePath: \cli\template\packages\openapi-v3\index.ts
  * @Description:
  *
  */
 import type { OpenAPIV3 } from 'openapi-types'
-import json from './OpenAPIV3.json'
+import json from './OpenAPIv3.json'
 
 export default json as OpenAPIV3.Document
