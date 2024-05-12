f90bf7d70a22607731cfc25706b56afbf91eb457
peerless_hero <peerless_hero@outlook.com>
2024-05-13 00:37:54 +0800

    feat: 版本号自动生成

--- src/version.ts
+++ src/version.ts
@@ -2,21 +2,43 @@
  * @Author: peerless_hero peerless_hero@outlook.com
  * @Date: 2024-05-10 00:25:28
  * @LastEditors: peerless_hero peerless_hero@outlook.com
- * @LastEditTime: 2024-05-10 02:29:35
+ * @LastEditTime: 2024-05-13 00:35:39
  * @FilePath: \cli\src\version.ts
  * @Description:
  *
  */
+import { execSync } from 'node:child_process'
+import { env, exit } from 'node:process'
 import consola from 'consola'
+import { inc } from 'semver'
+import { outputJSON, readJSON } from 'fs-extra/esm'
 import { name, version } from '../package.json'
 
-export function getVersion() {
+export function getCliVersion() {
   return version
 }
 
-export function outputVersion() {
-  const title = `${name} (v${version})`
+export const title = `${name} (v${version})`
+
+export function getPackageLatestVersion(pkgName?: string) {
+  if (!pkgName)
+    return ''
+
+  try {
+    const latestVersion = execSync(`npm view ${pkgName} version --silent`, { encoding: 'utf-8' })
+    return latestVersion
+  }
+  catch (err) {
+    // 找不到NPM包会发生在首次构建的时候，这里不用报错，返回空字符串即可
+    return ''
+  }
+}
+
+export async function outputVersion() {
+  const latestVersion = getPackageLatestVersion(name) || 'unknown'
   const message = [
+    `current version: \`${version}\``,
+    `latest version: \`${latestVersion}\``,
     '`api` - Generate api service template.',
     '`type` - Generate `.d.ts` files by OpenAPIv3.',
     '`request` - Generate request modules by OpenAPIv3.',
@@ -31,4 +53,50 @@ export function outputVersion() {
       borderStyle: 'rounded',
     },
   })
+}
+
+const {
+  PACKAGE_SCOPE,
+  PACKAGE_UN_NAME = 'un',
+  PACKAGE_AXIOS_NAME = 'axios',
+  PACKAGE_OPENAPI_V3_NAME = 'openapi-v3',
+  INITIAL_VERSION = '0.0.0',
+} = env
+
+function getNewVersion() {
+  let currentVersion = getPackageLatestVersion(`${PACKAGE_SCOPE}/${PACKAGE_OPENAPI_V3_NAME}`) || getPackageLatestVersion(`${PACKAGE_SCOPE}/${PACKAGE_AXIOS_NAME}`) || getPackageLatestVersion(`${PACKAGE_SCOPE}/${PACKAGE_UN_NAME}`)
+  if (!currentVersion) {
+    consola.info('当前版本号不存在，自动设置为初始版本号：', INITIAL_VERSION)
+    currentVersion = INITIAL_VERSION
+    return currentVersion
+  }
+
+  consola.info('当前版本号为：', currentVersion)
+  const newVersion = inc(currentVersion, 'patch')
+  if (!newVersion) {
+    consola.error('无法根据当前版本号自动生成新版本号')
+    exit()
+  }
+  consola.info('新版本号为：', newVersion)
+  return newVersion
+}
+
+async function changePackage(name: string, version: string) {
+  const res = await readJSON(`temp/${name}/package.json`)
+  res.name = `${PACKAGE_SCOPE}/${name}`
+  res.version = version
+  await outputJSON(`packages/${name}/package.json`, res, { spaces: 2 })
+}
+
+/**
+ *
+ * 将所有包的版本号更新为新版本号
+ */
+export function updateRequestVersion() {
+  const newVersion = getNewVersion()
+  return Promise.all([
+    changePackage(PACKAGE_UN_NAME, newVersion),
+    changePackage(PACKAGE_AXIOS_NAME, newVersion),
+    changePackage(PACKAGE_OPENAPI_V3_NAME, newVersion),
+  ])
 }
