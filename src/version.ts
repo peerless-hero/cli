93df5d7c63fbc31da1749ded66e0fba47869f8a6
peerless_hero <peerless_hero@outlook.com>
2024-11-21 00:59:37 +0800

    feat: 补丁版本号过大时，自动增加次版本号

--- src/version.ts
+++ src/version.ts
@@ -2,7 +2,7 @@
  * @Author: peerless_hero peerless_hero@outlook.com
  * @Date: 2024-05-10 00:25:28
  * @LastEditors: peerless_hero peerless_hero@outlook.com
- * @LastEditTime: 2024-05-13 02:23:31
+ * @LastEditTime: 2024-11-21 00:59:33
  * @FilePath: \cli\src\version.ts
  * @Description:
  *
@@ -11,7 +11,7 @@ import { execSync } from 'node:child_process'
 import { env, exit } from 'node:process'
 import { resolve } from 'node:path'
 import consola from 'consola'
-import { inc } from 'semver'
+import { parse } from 'semver'
 import { outputJSON, readJSON } from 'fs-extra/esm'
 import { name, version } from '../package.json'
 import { PACKAGE_AXIOS_PATH, PACKAGE_UN_PATH, TEMP_OPENAPI_V3_PATH } from './paths'
@@ -42,6 +42,7 @@ const {
   PACKAGE_AXIOS_NAME = 'axios',
   PACKAGE_OPENAPI_V3_NAME = 'openapi-v3',
   INITIAL_VERSION = '0.0.0',
+  MAX_PATCH_VERSION = '99',
 } = env
 
 async function changePackage(basePath: string, name: string, version: string) {
@@ -59,19 +60,31 @@ async function changePackage(basePath: string, name: string, version: string) {
  */
 export function updateRequestVersion() {
   consola.info('获取当前版本号')
-  let currentVersion = getPackageLatestVersion(`${PACKAGE_SCOPE}/${PACKAGE_OPENAPI_V3_NAME}`) || getPackageLatestVersion(`${PACKAGE_SCOPE}/${PACKAGE_AXIOS_NAME}`) || getPackageLatestVersion(`${PACKAGE_SCOPE}/${PACKAGE_UN_NAME}`)
-  if (!currentVersion) {
-    consola.info('当前版本号不存在，自动设置为初始版本号：', INITIAL_VERSION)
-    currentVersion = INITIAL_VERSION
+  const currentVersion = getPackageLatestVersion(`${PACKAGE_SCOPE}/${PACKAGE_OPENAPI_V3_NAME}`) || getPackageLatestVersion(`${PACKAGE_SCOPE}/${PACKAGE_AXIOS_NAME}`) || getPackageLatestVersion(`${PACKAGE_SCOPE}/${PACKAGE_UN_NAME}`)
+  let maxPatchVersion = Number(MAX_PATCH_VERSION)
+  // 最大补丁版本号不能为非正数
+  maxPatchVersion = maxPatchVersion > 0 ? maxPatchVersion : 99
+  let newVersion: string | null = null
+  if (currentVersion) {
+    consola.info('当前版本号为：', currentVersion)
+    const semver = parse(currentVersion)
+    if (!semver) {
+      consola.error('无法根据当前版本号自动生成新版本号')
+      exit()
+    }
+    if (semver.patch + 1 >= maxPatchVersion) {
+      consola.warn(`当前补丁版本号数值【${semver.patch}】已达到设定最大值【${maxPatchVersion}】，故次版本号将增加1`)
+      newVersion = semver.inc('minor').format()
+    }
+    else {
+      newVersion = semver.inc('patch').format()
+    }
+    consola.info('新版本号为：', newVersion)
   }
-
-  consola.info('当前版本号为：', currentVersion)
-  const newVersion = inc(currentVersion, 'patch')
-  if (!newVersion) {
-    consola.error('无法根据当前版本号自动生成新版本号')
-    exit()
+  else {
+    consola.info('无法从NPM获取当前版本号，故使用初始版本号作为当前版本号', INITIAL_VERSION)
+    newVersion = INITIAL_VERSION
   }
-  consola.info('新版本号为：', newVersion)
   return Promise.all([
     changePackage(PACKAGE_UN_PATH, PACKAGE_UN_NAME, newVersion),
     changePackage(PACKAGE_AXIOS_PATH, PACKAGE_AXIOS_NAME, newVersion),
