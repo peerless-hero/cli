f607584bc339959750b588f045ba83fdc57518e6
peerless_hero <peerless_hero@outlook.com>
2024-12-11 03:22:16 +0800

    feat(paths): 添加 CHANGELOG.md 临时路径
    - 新增 TEMP_CHANGELOG_PATH 常量，用于指定 CHANGELOG.md 的临时路径

--- src/paths.ts
+++ src/paths.ts
@@ -2,7 +2,7 @@
  * @Author: peerless_hero peerless_hero@outlook.com
  * @Date: 2024-05-09 22:25:18
  * @LastEditors: peerless_hero peerless_hero@outlook.com
- * @LastEditTime: 2024-05-14 00:22:16
+ * @LastEditTime: 2024-12-10 21:46:01
  * @FilePath: \cli\src\paths.ts
  * @Description:
  *
@@ -53,3 +53,8 @@ export const TEMP_UN_PATH = resolve('temp', PACKAGE_SCOPE, PACKAGE_UN_NAME)
  * `openapi-v3请求包`临时目录
  */
 export const TEMP_OPENAPI_V3_PATH = resolve('temp', PACKAGE_SCOPE, PACKAGE_OPENAPI_V3_NAME)
+
+/**
+ * CHANGELOG.md 临时路径
+ */
+export const TEMP_CHANGELOG_PATH = resolve('temp', 'CHANGELOG.md')
