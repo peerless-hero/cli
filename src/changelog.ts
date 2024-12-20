813d02983f886e46ed731197448ffb664e10ab9b
peerless_hero <peerless_hero@outlook.com>
2024-12-20 23:43:42 +0800

    feat(cli): 升级日志功能并添加 webhook 支持
    - 重构 generateMarkdown 函数，优化接口和类型比较逻辑
    - 新增 renderRequestChangelog 函数，支持自定义新版本文档和版本号
    - 添加 createWebhookDingTalk 和 createWebhookWeCom 函数，实现钉钉和企业微信 webhook 通知

--- src/changelog.ts
+++ src/changelog.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: peerless_hero peerless_hero@outlook.com
  * @Date: 2024-05-13 18:49:35
- * @LastEditors: zhaojinfeng 121016171@qq.com
- * @LastEditTime: 2024-12-11 14:41:40
+ * @LastEditors: peerless_hero peerless_hero@outlook.com
+ * @LastEditTime: 2024-12-20 23:39:18
  * @FilePath: \cli\src\changelog.ts
  * @Description:
  *
@@ -12,13 +12,15 @@ import { resolve } from 'node:path'
 import { renderFile } from 'ejs'
 import { outputFile, outputJSON } from 'fs-extra/esm'
 import consola from 'consola'
-import { TEMPLATE_DIR, TEMP_CHANGELOG_PATH } from './paths'
+import type { OpenAPIV3 } from 'openapi-types'
+import { TEMPLATE_DIR } from './paths'
 import getOpenapi3 from './openapi3'
 import { compareAPI } from './api'
 import { compareType } from './type'
-import 'dotenv/config'
+import { createWebhookDingTalk, createWebhookWeCom } from './webhook'
 import { checkApiEnv } from './env'
 import { getNewVersion, getPackageLatestVersion } from './version'
+import 'dotenv/config'
 
 interface Changelog {
   add: string[]
@@ -30,38 +32,63 @@ const TITLE_TEMPLATE = resolve(TEMPLATE_DIR, 'ejs/markdown/title.ejs')
 const CONTENT_TEMPLATE = resolve(TEMPLATE_DIR, 'ejs/markdown/api-changelog.ejs')
 const { PACKAGE_SCOPE, PACKAGE_OPENAPI_V3_NAME } = checkApiEnv()
 const { OLD_OPENAPI_DATASOURCE = 'module', OLD_OPENAPI_APIFOX_PROJECT_ID, CHANGELOG_OUTPUT_DIR = 'temp' } = env
-export async function generateMarkdown(api: Changelog, type: Changelog, infoTitle = PACKAGE_SCOPE) {
-  const currentVersion = getPackageLatestVersion(`${PACKAGE_SCOPE}/${PACKAGE_OPENAPI_V3_NAME}`)
-  const newVersion = getNewVersion(currentVersion)
 
+interface MarkdownOption {
+  api: Changelog
+  type: Changelog
+  newVersion: string
+  infoTitle?: string
+}
+export async function generateMarkdown({ api, type, infoTitle = PACKAGE_SCOPE, newVersion }: MarkdownOption) {
   const [title, content1, content2] = await Promise.all([
     renderFile(TITLE_TEMPLATE, { h1: `${infoTitle} ${newVersion}` }),
     renderFile(CONTENT_TEMPLATE, { changelog: api, type: '接口' }),
     renderFile(CONTENT_TEMPLATE, { changelog: type, type: '模型' }),
   ])
 
-  let content = content1 + content2
-  if (!content)
-    content += '暂无变动'
+  let text = content1 + content2
+  if (!text)
+    text += '暂无变动'
 
-  await outputFile(resolve(CHANGELOG_OUTPUT_DIR, 'CHANGELOG.json'), title + content)
+  await outputFile(resolve(CHANGELOG_OUTPUT_DIR, 'CHANGELOG.md'), title + text)
+  return { title, text }
 }
 
-export async function renderApiChangelog() {
+interface RequestChangelogOption {
+  /**
+   * 新版本文档
+   */
+  newDocument: OpenAPIV3.Document
+  /**
+   * 新版本版本号
+   */
+  newVersion: string
+}
+
+export async function renderRequestChangelog({ newDocument, newVersion }: RequestChangelogOption) {
   const oldDocument = await getOpenapi3(OLD_OPENAPI_DATASOURCE, OLD_OPENAPI_APIFOX_PROJECT_ID)
-  const newDocument = await getOpenapi3()
   const api = compareAPI(oldDocument, newDocument)
   const type = compareType(oldDocument, newDocument)
   consola.info('接口变动总数：', api.total)
   consola.info('模型变动总数：', type.total)
-  generateMarkdown(api, type, newDocument.info?.title)
+  const markdown = await generateMarkdown({ api, type, infoTitle: newDocument.info?.title, newVersion })
   if (argv.includes('--changelog-debug')) {
     outputJSON('temp/newDocument.json', newDocument)
     outputJSON('temp/oldDocument.json', oldDocument)
-    outputJSON(resolve(CHANGELOG_OUTPUT_DIR, 'CHANGELOG.md'), { api, type })
+    outputJSON(resolve(CHANGELOG_OUTPUT_DIR, 'CHANGELOG.json'), { api, type })
   }
+  if (argv.includes('--webhook-wecom'))
+    createWebhookWeCom(markdown)
+
+  if (argv.includes('--webhook-dingtalk'))
+    createWebhookDingTalk(markdown)
 }
 
-export function renderChangelog() {
-  renderApiChangelog()
+export async function renderChangelog() {
+  const currentVersion = getPackageLatestVersion(`${PACKAGE_SCOPE}/${PACKAGE_OPENAPI_V3_NAME}`)
+  const newVersion = getNewVersion(currentVersion)
+
+  const newDocument = await getOpenapi3(OLD_OPENAPI_DATASOURCE, OLD_OPENAPI_APIFOX_PROJECT_ID)
+
+  renderRequestChangelog({ newDocument, newVersion })
 }
