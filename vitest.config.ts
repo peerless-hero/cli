cd1da058da28a1d6dd4c1e2046aa037abcacd1c5
peerless_hero <peerless_hero@outlook.com>
2026-07-01 21:21:20 +0800

    chore(vitest): 移除 setupFiles 配置

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,15 @@
+/*
+ * @Author: peerless_hero peerless_hero@outlook.com
+ * @Date: 2026-07-01 03:12:22
+ * @LastEditors: peerless_hero peerless_hero@outlook.com
+ * @LastEditTime: 2026-07-01 21:20:45
+ * @FilePath: \cli\vitest.config.ts
+ * @Description: vitest 配置文件
+ */
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    setupFiles: ['./src/test-setup.ts'],
     environment: 'node',
     include: ['src/**/*.test.ts'],
   },
